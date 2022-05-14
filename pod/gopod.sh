6662f089fef5569fc029ae1aff408303fcb81b1f
chenbin <15811514091@163.com>
2022-05-14 15:17:59 +0800

    Update pod shells

--- pod/gopod.sh
+++ pod/gopod.sh
@@ -5,7 +5,6 @@ APP_NAME="knowledge-build-server"
 
 # 查看部署的服务pod name
 #kubectl get pod -A | grep $APP_NAME
-
 POD_NAME=$(kubectl get pod -A | grep $APP_NAME | awk -F " " '{print $1 " " $2}')
 echo $POD_NAME
 
